9ecf58fb457807ac6d4bb3fafa7223c916e6621b
temitope777 <42391358+temitope777@users.noreply.github.com>
2018-08-14 23:05:45 +0100

    Update nodejsdocker.groovy

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('temitope777/devopsrepository')
+            repositoryName('tope777/devopsstudy')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
